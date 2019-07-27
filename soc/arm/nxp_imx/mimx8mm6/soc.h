ec842221a0fc15800e14e6a54fd38ea1d9abcdf3
Manivannan Sadhasivam <mani@kernel.org>
2019-07-27 12:08:00 +0530

    soc: arm: nxp_imx: Add i.MX8M SoC support
    Add SoC support for NXP i.MX8M SoC.
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

---
+++ soc/arm/nxp_imx/mimx8mm6/soc.h
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2019, Manivannan Sadhasivam <mani@kernel.org>
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef _SOC__H_
+#define _SOC__H_
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef _ASMLANGUAGE
+
+#endif /* !_ASMLANGUAGE */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* _SOC__H_ */
