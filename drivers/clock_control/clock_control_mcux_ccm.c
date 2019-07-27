40458253f0a2ab358c3ae4f8f8a6542adc23a77f
Manivannan Sadhasivam <mani@kernel.org>
2019-07-27 12:17:33 +0530

    drivers: clock_control: Add support for CCM UART clock
    Add support for CCM UART clock based on NXP MCUX SDK.
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

--- drivers/clock_control/clock_control_mcux_ccm.c
+++ drivers/clock_control/clock_control_mcux_ccm.c
@@ -13,12 +13,14 @@
 #include <logging/log.h>
 LOG_MODULE_REGISTER(clock_control);
 
+#ifdef CONFIG_SPI_MCUX_LPSPI
 static const clock_name_t lpspi_clocks[] = {
 	kCLOCK_Usb1PllPfd1Clk,
 	kCLOCK_Usb1PllPfd0Clk,
 	kCLOCK_SysPllClk,
 	kCLOCK_SysPllPfd2Clk,
 };
+#endif
 
 static int mcux_ccm_on(struct device *dev,
 			      clock_control_subsys_t sub_system)
@@ -53,6 +55,7 @@ static int mcux_ccm_get_subsys_rate(struct device *dev,
 		break;
 #endif
 
+#ifdef CONFIG_SPI_MCUX_LPSPI
 	case IMX_CCM_LPSPI_CLK:
 	{
 		u32_t lpspi_mux = CLOCK_GetMux(kCLOCK_LpspiMux);
@@ -62,7 +65,9 @@ static int mcux_ccm_get_subsys_rate(struct device *dev,
 			/ (CLOCK_GetDiv(kCLOCK_LpspiDiv) + 1);
 		break;
 	}
+#endif
 
+#ifdef CONFIG_UART_MCUX_LPUART
 	case IMX_CCM_LPUART_CLK:
 		if (CLOCK_GetMux(kCLOCK_UartMux) == 0) {
 			*rate = CLOCK_GetPllFreq(kCLOCK_PllUsb1) / 6
@@ -73,6 +78,7 @@ static int mcux_ccm_get_subsys_rate(struct device *dev,
 		}
 
 		break;
+#endif
 
 #ifdef CONFIG_DISK_ACCESS_USDHC1
 	case IMX_CCM_USDHC1_CLK:
@@ -87,6 +93,9 @@ static int mcux_ccm_get_subsys_rate(struct device *dev,
 				(CLOCK_GetDiv(kCLOCK_Usdhc2Div) + 1U);
 		break;
 #endif
+	case IMX_CCM_UART_CLK:
+		*rate = CLOCK_GetPllFreq(kCLOCK_SystemPll1Ctrl) / (CLOCK_GetRootPreDivider(kCLOCK_RootUart4)) / (CLOCK_GetRootPostDivider(kCLOCK_RootUart4)) / 10;
+		break;
 	}
 
 	return 0;
