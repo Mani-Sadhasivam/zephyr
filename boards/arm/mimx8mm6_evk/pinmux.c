8c30787a4a526cdc633645101be29af88114f014
Manivannan Sadhasivam <mani@kernel.org>
2019-08-07 08:13:29 +0530

    boards: arm: mimx8mm6_evk: Enable I2C3
    Enable I2C3 exposed on the mimx8mm6_evk.
    
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

--- boards/arm/mimx8mm6_evk/pinmux.c
+++ boards/arm/mimx8mm6_evk/pinmux.c
@@ -11,6 +11,19 @@ static int mimx8mm6_evk_pinmux_init(struct device *dev)
 {
 	ARG_UNUSED(dev);
 
+#ifdef CONFIG_I2C_3
+    IOMUXC_SetPinMux(IOMUXC_I2C3_SCL_I2C3_SCL, 1U);
+    IOMUXC_SetPinConfig(IOMUXC_I2C3_SCL_I2C3_SCL,
+                        IOMUXC_SW_PAD_CTL_PAD_DSE(6U) |
+                        IOMUXC_SW_PAD_CTL_PAD_FSEL(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
+    IOMUXC_SetPinMux(IOMUXC_I2C3_SDA_I2C3_SDA, 1U);
+    IOMUXC_SetPinConfig(IOMUXC_I2C3_SDA_I2C3_SDA,
+                        IOMUXC_SW_PAD_CTL_PAD_DSE(6U) |
+                        IOMUXC_SW_PAD_CTL_PAD_FSEL(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
+#endif
+
 #ifdef CONFIG_UART_MCUX_IMX_4
     IOMUXC_SetPinMux(IOMUXC_UART4_RXD_UART4_RX, 0U);
     IOMUXC_SetPinConfig(IOMUXC_UART4_RXD_UART4_RX,
