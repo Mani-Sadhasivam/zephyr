50135660738a1307b718077c36cf5fe2071316ae
Manivannan Sadhasivam <mani@kernel.org>
2019-08-16 00:25:00 +0530

    boards: arm: Enable I2S_3 on i.MX8M EVK
    Signed-off-by: Manivannan Sadhasivam <mani@kernel.org>

--- boards/arm/mimx8mm6_evk/pinmux.c
+++ boards/arm/mimx8mm6_evk/pinmux.c
@@ -24,6 +24,30 @@ static int mimx8mm6_evk_pinmux_init(struct device *dev)
                         IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
 #endif
 
+#ifdef CONFIG_I2S_3
+    IOMUXC_SetPinMux(IOMUXC_I2C4_SDA_GPIO5_IO21, 1U);
+    IOMUXC_SetPinMux(IOMUXC_SAI3_MCLK_SAI3_MCLK, 0U);
+    IOMUXC_SetPinConfig(IOMUXC_SAI3_MCLK_SAI3_MCLK,
+                        IOMUXC_SW_PAD_CTL_PAD_DSE(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_FSEL(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
+    IOMUXC_SetPinMux(IOMUXC_SAI3_TXC_SAI3_TX_BCLK, 0U);
+    IOMUXC_SetPinConfig(IOMUXC_SAI3_TXC_SAI3_TX_BCLK,
+                        IOMUXC_SW_PAD_CTL_PAD_DSE(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_FSEL(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
+    IOMUXC_SetPinMux(IOMUXC_SAI3_TXD_SAI3_TX_DATA0, 0U);
+    IOMUXC_SetPinConfig(IOMUXC_SAI3_TXD_SAI3_TX_DATA0,
+                        IOMUXC_SW_PAD_CTL_PAD_DSE(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_FSEL(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
+    IOMUXC_SetPinMux(IOMUXC_SAI3_TXFS_SAI3_TX_SYNC, 0U);
+    IOMUXC_SetPinConfig(IOMUXC_SAI3_TXFS_SAI3_TX_SYNC,
+                        IOMUXC_SW_PAD_CTL_PAD_DSE(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_FSEL(2U) |
+                        IOMUXC_SW_PAD_CTL_PAD_HYS_MASK);
+#endif
+
 #ifdef CONFIG_UART_MCUX_IMX_4
     IOMUXC_SetPinMux(IOMUXC_UART4_RXD_UART4_RX, 0U);
     IOMUXC_SetPinConfig(IOMUXC_UART4_RXD_UART4_RX,
