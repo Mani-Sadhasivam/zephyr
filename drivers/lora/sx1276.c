160cfc1359098b95f15c20f6812ba998c507e7cc
Manivannan Sadhasivam <mani@kernel.org>
2019-09-07 00:08:08 +0530

    temp2

--- drivers/lora/sx1276.c
+++ drivers/lora/sx1276.c
@@ -8,8 +8,10 @@
 #include <spi.h>
 #include <misc/util.h>
 #include <net/lora.h>
+#include <rtc.h>
 #include <zephyr.h>
 #include <sx1276/sx1276.h>
+#include <arch/arm/cortex_m/cmsis.h>
 
 #define LOG_LEVEL CONFIG_LORA_LOG_LEVEL
 #include <logging/log.h>
@@ -22,6 +24,8 @@ LOG_MODULE_REGISTER(sx1276);
 #define CLOCK_FREQ		DT_INST_0_SEMTECH_SX1276_CLOCK_FREQUENCY
 
 #define SX1276_REG_VERSION			0x42
+
+extern DioIrqHandler *DioIrq[];
 
 int sx1276_dio_pins[SX1276_MAX_DIO] = {
 	DT_INST_0_SEMTECH_SX1276_DIO_GPIOS_PIN_0,
@@ -41,21 +45,126 @@ static char sx1276_dio_ports[SX1276_MAX_DIO][6] = {
 
 struct sx1276_data {
 	struct device *spi;
+	struct device *rtc;
+	struct device *reset;
 	struct spi_config spi_cfg;
 	struct device *dio_dev[SX1276_MAX_DIO];
 	struct gpio_callback irq_cb;
 	struct k_sem data_sem;
 } dev_data;
 
-bool SX1276CheckRfFrequency( uint32_t frequency )
+bool SX1276CheckRfFrequency(uint32_t frequency)
 {
     // Implement check. Currently all frequencies are supported
     return true;
 }
 
+void RtcStopAlarm(void)
+{
+	rtc_disable(dev_data.rtc);
+}
+
+void SX1276SetAntSwLowPower( bool status )
+{   
+	/* Not implemented */
+}
+
+void SX1276SetBoardTcxo( uint8_t state )
+{
+	/* Not implemented */
+}
+
+void SX1276SetAntSw( uint8_t opMode )
+{
+	/* Not implemented */
+}
+
+void SX1276Reset( void )
+{
+	gpio_pin_configure(dev_data.reset, GPIO_RESET_PIN,
+			   GPIO_DIR_OUT | GPIO_PUD_NORMAL);
+	gpio_pin_write(dev_data.reset, GPIO_RESET_PIN, 0);
+	k_sleep(1);
+	gpio_pin_configure(dev_data.reset, GPIO_RESET_PIN,
+			   GPIO_DIR_IN | GPIO_PUD_NORMAL);
+	k_sleep(6);
+}
+
+void BoardCriticalSectionBegin(uint32_t *mask)
+{
+    *mask = __get_PRIMASK( );
+    __disable_irq( );
+}
+
+void BoardCriticalSectionEnd( uint32_t *mask )
+{
+    __set_PRIMASK( *mask );
+}
+
+uint32_t RtcGetTimerElapsedTime(void)
+{
+	return rtc_read(dev_data.rtc);
+}
+
+u32_t RtcGetMinimumTimeout(void)
+{
+	return 3;
+}
+
+void RtcSetAlarm(uint32_t timeout)
+{
+	rtc_set_alarm(dev_data.rtc, timeout);
+}
+
+uint32_t RtcSetTimerContext(void)
+{
+	struct rtc_config config;
+
+	config.init_val = 0;
+	rtc_set_config(dev_data.rtc, &config);
+}
+
 static void sx1276_irq_callback(struct device *dev,
 				struct gpio_callback *cb, u32_t pins)
 {
+	unsigned int i, pin;
+
+	for (i = 0; i < SX1276_MAX_DIO; i++) {
+		pin = BIT(pins);
+		if (pin == sx1276_dio_pins[i]) {
+			(*DioIrq[i])(NULL);
+		}
+	}
+}
+
+void SX1276IoIrqInit(DioIrqHandler **irqHandlers)
+{
+	unsigned int i;
+
+	/* Setup DIO gpios */
+	for (i = 0; i < SX1276_MAX_DIO; i++) {
+		dev_data.dio_dev[i] = device_get_binding(sx1276_dio_ports[i]);
+		if (dev_data.dio_dev[i] == NULL) {
+			LOG_ERR("Cannot get pointer to %s device",
+				sx1276_dio_ports[i]);
+			return;
+		}
+
+		gpio_pin_configure(dev_data.dio_dev[i], sx1276_dio_pins[i],
+				   GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE |
+				   GPIO_INT_DEBOUNCE | GPIO_INT_ACTIVE_HIGH);
+
+		gpio_init_callback(&dev_data.irq_cb,
+				   sx1276_irq_callback,
+				   BIT(sx1276_dio_pins[i]));
+
+		if (gpio_add_callback(dev_data.dio_dev[i], &dev_data.irq_cb) < 0) {
+			LOG_ERR("Could not set gpio callback.");
+			return;
+		}
+		gpio_pin_enable_callback(dev_data.dio_dev[i], sx1276_dio_pins[i]);
+	}
+
 }
 
 static int sx1276_transceive(u8_t reg, bool write, void *data, size_t length)
@@ -140,90 +249,15 @@ static int sx1276_lora_recv(struct device *dev, u8_t *data)
 static int sx1276_lora_config(struct device *dev,
 			      struct lora_modem_config *config)
 {
-	return 0;
-}
 
-static int sx1276_lora_init(struct device *dev)
-{
-	static struct spi_cs_control spi_cs;
-	struct device *reset_dev;
-	int ret;
-	unsigned int i;
-	u8_t regval;
-
-	dev_data.spi = device_get_binding(DT_INST_0_SEMTECH_SX1276_BUS_NAME);
-	if (!dev_data.spi) {
-		LOG_ERR("spi device not found: %s",
-			    DT_INST_0_SEMTECH_SX1276_BUS_NAME);
-		return -EINVAL;
-	}
-
-	dev_data.spi_cfg.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB;
-	dev_data.spi_cfg.frequency = DT_INST_0_SEMTECH_SX1276_SPI_MAX_FREQUENCY;
-	dev_data.spi_cfg.slave = DT_INST_0_SEMTECH_SX1276_BASE_ADDRESS;
-
-	spi_cs.gpio_pin = GPIO_CS_PIN,
-	spi_cs.gpio_dev = device_get_binding(
-			DT_INST_0_SEMTECH_SX1276_CS_GPIOS_CONTROLLER);
-	if (!spi_cs.gpio_dev) {
-		LOG_ERR("Failed to initialize CS GPIO driver: %s",
-		       DT_INST_0_SEMTECH_SX1276_CS_GPIOS_CONTROLLER);
-		return -EIO;
-	}
-
-	dev_data.spi_cfg.cs = &spi_cs;
-
-	/* Setup DIO gpios */
-	for (i = 0; i < SX1276_MAX_DIO; i++) {
-		dev_data.dio_dev[i] = device_get_binding(sx1276_dio_ports[i]);
-		if (dev_data.dio_dev[i] == NULL) {
-			LOG_ERR("Cannot get pointer to %s device",
-				sx1276_dio_ports[i]);
-			return -EINVAL;
-		}
-
-		gpio_pin_configure(dev_data.dio_dev[i], sx1276_dio_pins[i],
-				   GPIO_DIR_IN | GPIO_INT | GPIO_INT_EDGE |
-				   GPIO_INT_DEBOUNCE | GPIO_INT_ACTIVE_HIGH);
-
-		gpio_init_callback(&dev_data.irq_cb,
-				   sx1276_irq_callback,
-				   BIT(sx1276_dio_pins[i]));
-
-		if (gpio_add_callback(dev_data.dio_dev[i], &dev_data.irq_cb) < 0) {
-			LOG_ERR("Could not set gpio callback.");
-			return -EIO;
-		}
-		gpio_pin_enable_callback(dev_data.dio_dev[i], sx1276_dio_pins[i]);
-	}
-
-	/* Setup Reset gpio */
-	reset_dev = device_get_binding(
-			DT_INST_0_SEMTECH_SX1276_RESET_GPIOS_CONTROLLER);
-	if (!reset_dev) {
-		LOG_ERR("Failed to initialize Reset GPIO driver: %s",
-		       DT_INST_0_SEMTECH_SX1276_RESET_GPIOS_CONTROLLER);
-		return -EIO;
-	}
-
-	ret = gpio_pin_configure(reset_dev, GPIO_RESET_PIN, GPIO_DIR_OUT);
-
-	/* Perform soft reset */
-	gpio_pin_write(reset_dev, GPIO_RESET_PIN, 0);
-	k_sleep(100);
-	gpio_pin_write(reset_dev, GPIO_RESET_PIN, 1);
-	k_sleep(100);
-
-	ret = sx1276_read(SX1276_REG_VERSION, &regval, 1);
-	if (ret < 0) {
-		LOG_ERR("Unable to read version info");
-		return -EIO;
-	}
-
-	k_sem_init(&dev_data.data_sem, 0, UINT_MAX);
-
-	LOG_INF("SX1276 Version:%02x found", regval);
-
+/*
+	data->sx1276_config.lorap2p_param.Frequency = config->frequency;
+	data->sx1276_config.lorap2p_param.Spreadfact = config->spreading_factor;
+	data->sx1276_config.lorap2p_param.Bandwidth = config->bandwidth;
+	data->sx1276_config.lorap2p_param.Codingrate = config->coding_rate;
+	data->sx1276_config.lorap2p_param.Preamlen = config->preamble_len;
+	data->sx1276_config.lorap2p_param.Powerdbm = config->power;
+*/
 	return 0;
 }
 
@@ -257,6 +291,73 @@ const struct Radio_s Radio = {
 	.RxBoosted = NULL,
 	.SetRxDutyCycle = NULL,
 };
+
+static int sx1276_lora_init(struct device *dev)
+{
+	static struct spi_cs_control spi_cs;
+	int ret;
+	u8_t regval;
+
+	dev_data.spi = device_get_binding(DT_INST_0_SEMTECH_SX1276_BUS_NAME);
+	if (!dev_data.spi) {
+		LOG_ERR("Cannot get pointer to %s device",
+			    DT_INST_0_SEMTECH_SX1276_BUS_NAME);
+		return -EINVAL;
+	}
+
+	dev_data.spi_cfg.operation = SPI_WORD_SET(8) | SPI_TRANSFER_MSB;
+	dev_data.spi_cfg.frequency = DT_INST_0_SEMTECH_SX1276_SPI_MAX_FREQUENCY;
+	dev_data.spi_cfg.slave = DT_INST_0_SEMTECH_SX1276_BASE_ADDRESS;
+
+	spi_cs.gpio_pin = GPIO_CS_PIN,
+	spi_cs.gpio_dev = device_get_binding(
+			DT_INST_0_SEMTECH_SX1276_CS_GPIOS_CONTROLLER);
+	if (!spi_cs.gpio_dev) {
+		LOG_ERR("Cannot get pointer to %s device",
+		       DT_INST_0_SEMTECH_SX1276_CS_GPIOS_CONTROLLER);
+		return -EIO;
+	}
+
+	dev_data.spi_cfg.cs = &spi_cs;
+
+	/* Setup Reset gpio */
+	dev_data.reset = device_get_binding(
+			DT_INST_0_SEMTECH_SX1276_RESET_GPIOS_CONTROLLER);
+	if (!dev_data.reset) {
+		LOG_ERR("Cannot get pointer to %s device",
+		       DT_INST_0_SEMTECH_SX1276_RESET_GPIOS_CONTROLLER);
+		return -EIO;
+	}
+
+	ret = gpio_pin_configure(dev_data.reset, GPIO_RESET_PIN, GPIO_DIR_OUT);
+
+	/* Perform soft reset */
+	gpio_pin_write(dev_data.reset, GPIO_RESET_PIN, 0);
+	k_sleep(100);
+	gpio_pin_write(dev_data.reset, GPIO_RESET_PIN, 1);
+	k_sleep(100);
+
+	ret = sx1276_read(SX1276_REG_VERSION, &regval, 1);
+	if (ret < 0) {
+		LOG_ERR("Unable to read version info");
+		return -EIO;
+	}
+
+	dev_data.rtc = device_get_binding(CONFIG_RTC_0_NAME);
+	if (!dev_data.rtc) {
+		LOG_ERR("Cannot get pointer to %s device",
+			CONFIG_RTC_0_NAME);
+		return -EIO;
+	}
+
+	k_sem_init(&dev_data.data_sem, 0, UINT_MAX);
+
+	Radio.Init(NULL);
+
+	LOG_INF("SX1276 Version:%02x found", regval);
+
+	return 0;
+}
 
 static const struct lora_driver_api sx1276_lora_api = {
 	.config = sx1276_lora_config,
